7e453bdaf6189a8ddc14bbcf322c9070c56c6745
Kerson Veloso <kerson213@gmail.com>
2019-08-29 11:03:00 +0100

    Add files via upload

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do weigh? "
+weight = gets.chomp
+
+puts "So, your #{age} old, #{height} tall and #{weight} heavy"
